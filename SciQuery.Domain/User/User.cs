6d33c95822a5479be70a4e6add1315ba134a3464
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2024-07-29 14:25:38 +0500

    Nimadr qildm

--- SciQuery.Domain/User/User.cs
+++ SciQuery.Domain/User/User.cs
@@ -4,5 +4,8 @@ namespace SciQuery.Domain.User;
 
 public class User : IdentityUser
 {
-
+    public string ProfileImagePath { get; set; }
+    public int Reputation {  get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastLogindate {  get; set; }
 }
