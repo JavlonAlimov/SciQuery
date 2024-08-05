994798b57123fb6cae5c7a8c33d50ff3647c18db
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2024-08-05 16:12:50 +0500

    asdfgd

--- SciQuery.Infrastructure/Persistance/DbContext/SciQueryDbContext.cs
+++ SciQuery.Infrastructure/Persistance/DbContext/SciQueryDbContext.cs
@@ -78,7 +78,7 @@ public class SciQueryDbContext(DbContextOptions<SciQueryDbContext> options,
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnectionJavlon"));
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnectionMuhammad"));
         base.OnConfiguring(optionsBuilder);
     }
 }
